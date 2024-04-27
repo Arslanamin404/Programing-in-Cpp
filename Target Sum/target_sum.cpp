65f92e653f43a597ca1053cee22ffdf3dba4f522
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2024-04-27 18:13:28 +0530

    Target Sum Based Problems

---
+++ Target Sum/target_sum.cpp
@@ -0,0 +1,38 @@
+// Find the total number of pairs in the array whose sum is equal to the given value x
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, size, pairs = 0;
+    cout << "Size of array: ";
+    cin >> size;
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\nValue of X: ";
+    cin >> x;
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
+        for (int j = i + 1; j < sizeof(arr) / sizeof(arr[0]); j++)
+        {
+            if (arr[i] + arr[j] == x)
+            {
+                cout << "(" << arr[i] << "," << arr[j] << ")";
+                pairs++;
+            }
+        }
+    }
+    cout << "\nTheir are " << pairs << " total number of pairs whose sum = " << x << endl;
+    return 0;
+}
